44671c6827aed0218274d3f209a63d82c86b6902
Sviatoslav Samoilenko <Sviatoslav_Samoilenko@epam.com>
2022-06-27 21:02:48 +0300

    Added demo of logic extraction

--- Lection2_27_06/Program.cs
+++ Lection2_27_06/Program.cs
@@ -4,9 +4,41 @@ namespace Lection2_27_06
 {
     class Program
     {
+        static int ConvertHoursToMinutes(int numberOfHours)
+        {
+            return numberOfHours * 60;
+        }
+
+        static float CalculateEquation(int a, int b)
+        {
+            return (float)(5 * a + b * b) / (b - a);
+        }
+
+        static void Swap(ref string a, ref string b)
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Test(ref int a)
+        {
+            a = 100;
+        }
+
+        static void AnotherTest(out int a)
+        {
+            a = 10;
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("Hello world!");
+            string a = "Hello";
+            string b = "World";
+
+            Swap(ref a, ref b);
+
+            Console.WriteLine(a + b);
         }
     }
 }
